6c4de800816371abec25cc9abd326542761239ce
nguyenvandiatlvn3@gmail.com <nguyenvandiatlvn3@gmail.com>
2022-11-08 22:49:05 +0700

    Init project

---
+++ src/main/java/com/proxtechshop/ProtechShopApplication.java
@@ -0,0 +1,13 @@
+package com.proxtechshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtechShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtechShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proxtechshop/ProtechShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxtechshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtechShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
