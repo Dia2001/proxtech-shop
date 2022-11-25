ea70dbdab89e23606d826886c79da3a6403c5df3
nguyenvandiatlvn3@gmail.com <nguyenvandiatlvn3@gmail.com>
2022-11-25 18:13:10 +0700

    feat: add package enum and random oderid

--- src/main/java/com/proxtechshop/utils/Utils.java
+++ src/main/java/com/proxtechshop/utils/Utils.java
@@ -1,5 +1,7 @@
 package com.proxtechshop.utils;
 
+import java.util.Random;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class Utils {
@@ -9,5 +11,28 @@ public class Utils {
 		return bCryptPasswordEncoder;
 	}
 	
-	
+	public static String randomOrderId() {
+		String myString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "0123456789";
+
+		int targetStringLength = 10;
+		Random randomNumber = new Random();
+		StringBuilder buffer = new StringBuilder(targetStringLength);
+
+		for (int i = 0; i < targetStringLength; i++) {
+			if (i == 0) {
+				int randomNo = randomNumber.nextInt(myString.length());
+				// 5
+				Character character = myString.charAt(randomNo);
+				buffer.append(Character.toUpperCase(character));
+			} else {
+				StringBuilder sb = new StringBuilder(10);
+				int index = (int) (AlphaNumericString.length() * Math.random());
+				buffer.append(AlphaNumericString.charAt(index));
+			}
+		}
+		String generatedString = buffer.toString();
+	    return generatedString;
+	}
+
 }
